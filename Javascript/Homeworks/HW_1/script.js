9d4dde130c61db4f69381b75eb5fa4d3eb94f658
VLAD OSTAFIYCHUK <vladua28@gmail.com>
2023-05-01 22:46:21 +0300

    Commit

---
+++ Javascript/Homeworks/HW_1/script.js
@@ -0,0 +1,52 @@
+/*                      1                      */
+let a = 'hello';
+console.log(a);
+let b = 'owu';
+console.log(b);
+let c = 'com';
+console.log(c);
+let d = 'ua';
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+let i = 3.14;
+console.log(i);
+let j = 2.7;
+console.log(j);
+let k = 16;
+console.log(k);
+let l = true;
+console.log(l);
+let m = false;
+console.log(m);
+
+/*                      2                      */
+let firstName = 'Vladyslav';
+let middleName = 'Marianovych';
+let lastName = 'Ostafiychuk';
+
+let person = firstName + middleName + lastName;
+console.log(person);
+console.log(`My name is ${firstName} ${middleName} ${lastName}.`);
+
+/*                      3                      */
+let n = 100;
+console.log(typeof n);
+let o = '100';
+console.log(typeof o);
+let p = true;
+console.log(typeof p);
+
+/*                      4                      */
+
+let name = prompt('What is your name', "");
+let patronymic = prompt('What is your patronymic name?', "");
+let age = prompt('What is your age?', "");
+alert(`Your name is ${name}, your patronymic name is ${patronymic} and you are ${age} years old.`);
+console.log(`Your name is ${name}, your patronymic name is ${patronymic} and you are ${age} years old.`);
