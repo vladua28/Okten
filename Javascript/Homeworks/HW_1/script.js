ef0f3f01274c8ee4d9d8b663880aa60abc0440aa
VLAD OSTAFIYCHUK <vladua28@gmail.com>
2023-05-02 11:45:59 +0300

    Commit

--- Javascript/Homeworks/HW_1/script.js
+++ Javascript/Homeworks/HW_1/script.js
@@ -1,4 +1,5 @@
-/*                      1                      */
+/*1. Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+Вивести кожну змінну за допомогою: console.log*/
 let a = 'hello';
 console.log(a);
 let b = 'owu';
@@ -26,7 +27,7 @@ console.log(l);
 let m = false;
 console.log(m);
 
-/*                      2                      */
+/*2. Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)*/
 let firstName = 'Vladyslav';
 let middleName = 'Marianovych';
 let lastName = 'Ostafiychuk';
@@ -35,7 +36,8 @@ let person = firstName + middleName + lastName;
 console.log(person);
 console.log(`My name is ${firstName} ${middleName} ${lastName}.`);
 
-/*                      3                      */
+/*3. За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;*/
 let n = 100;
 console.log(typeof n);
 let o = '100';
@@ -43,10 +45,9 @@ console.log(typeof o);
 let p = true;
 console.log(typeof p);
 
-/*                      4                      */
-
+/*4. За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль*/
 let name = prompt('What is your name?', "");
 let patronymic = prompt('What is your patronymic name?', "");
-let age = prompt('What is your age?', "");
+let age = +prompt('What is your age?', "");
 alert(`Your name is ${name}, your patronymic name is ${patronymic} and you are ${age} years old.`);
 console.log(`Your name is ${name}, your patronymic name is ${patronymic} and you are ${age} years old.`);
